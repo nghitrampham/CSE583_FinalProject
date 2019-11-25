f051751c9a2eefec0faea4503c1c9d3ab5f8b63a
martaw22 <martaw22@gmail.com>
2019-11-25 15:16:30 -0800

    Updated doc strings

--- DeathRate_Data/test_healthfunctions.py
+++ DeathRate_Data/test_healthfunctions.py
@@ -1,21 +1,21 @@
-'''Tests the functions in healthdata_module.py'''
+'''Tests the functions in healthdata_module.py, which are used to clean the health data'''
 import pandas as pd
 import healthdata_module
 import healthdata_cleanup
 
 #Test data
-df1 = pd.read_csv('test_data_1.csv')
-df2 = pd.read_csv('test_data_2.csv')
+DF1 = pd.read_csv('test_data_1.csv')
+DF2 = pd.read_csv('test_data_2.csv')
 
 def test_func_changing_names():
     '''Testing the first function, renaming columns.'''
-    df_renamed = healthdata_module.changing_col_name(df1, 'Year', 'year')
+    df_renamed = healthdata_module.changing_col_name(DF1, 'Year', 'year')
     assert 'year' in list(df_renamed.columns)
     assert 'Year' not in list(df_renamed.columns)
 
 def test_func_split_col():
     '''Testing the second function, splitting a column value into two strings.'''
-    df_split = healthdata_module.split_column(df2, 'County', 'County', 'State Abrev', ',')
+    df_split = healthdata_module.split_column(DF2, 'County', 'County', 'State Abrev', ',')
     assert 'State Abrev' in list(df_split.columns)
     assert ' WA' in list(df_split['State Abrev'])
     assert 'King' in list(df_split['County'])
@@ -28,5 +28,5 @@ def test_func_choose_data():
 
 def test_func_concat():
     '''Testing the fourth function, which concatenates the functions vertically.'''
-    df_concat = healthdata_module.concat_dfs_vertically(df1, df2)
+    df_concat = healthdata_module.concat_dfs_vertically(DF1, DF2)
     assert len(df_concat) == 4
