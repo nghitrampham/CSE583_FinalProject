0f9ed59713ef68d5fba031b4d9cf13d9d714b5ae
martaw22 <martaw22@gmail.com>
2019-12-05 11:56:05 -0800

    saved folder under temporary name

---
+++ air_pollution_death_rate_related/temp/test_healthfunctions.py
@@ -0,0 +1,32 @@
+'''Tests the functions in healthdata_module.py, which are used to clean the health data'''
+import pandas as pd
+from ..scripts import healthdata_module
+from ..scripts import healthdata_cleanup
+
+#Test data
+DF1 = pd.read_csv('air_pollution_death_rate_related/Data/test_data_1.csv')
+DF2 = pd.read_csv('air_pollution_death_rate_related/Data/test_data_2.csv')
+
+def test_func_changing_names():
+    '''Testing the first function, renaming columns.'''
+    df_renamed = healthdata_module.changing_col_name(DF1, 'Year', 'year')
+    assert 'year' in list(df_renamed.columns)
+    assert 'Year' not in list(df_renamed.columns)
+
+def test_func_split_col():
+    '''Testing the second function, splitting a column value into two strings.'''
+    df_split = healthdata_module.split_column(DF2, 'County', 'County', 'State Abrev', ',')
+    assert 'State Abrev' in list(df_split.columns)
+    assert ' WA' in list(df_split['State Abrev'])
+    assert 'King' in list(df_split['County'])
+
+def test_func_choose_data():
+    '''Testing the third function, choosing data by year.'''
+    df_2015 = healthdata_module.choose_data_by_year(healthdata_cleanup.COUNTY_POP_MERGE, 2015)
+    assert '2015' in list(df_2015['Year'])
+    assert '2001' not in list(df_2015['Year'])
+
+def test_func_concat():
+    '''Testing the fourth function, which concatenates the functions vertically.'''
+    df_concat = healthdata_module.concat_dfs_vertically(DF1, DF2)
+    assert len(df_concat) == 4
---
+++ air_pollution_death_rate_related/temp/test_main_AQI.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import time
+import warnings
+import math
+import matplotlib.pyplot as plt
+import argparse
+import pickle
+import datetime as dt
+import unittest
+import pytest
+
+
+from numpy import newaxis
+from ..scripts.Air_Pollution import helpers
+from ..scripts.Air_Pollution import main_AQI
+from pandas import read_csv
+from pandas import DataFrame
+from pandas import concat
+from keras.models import load_model
+
+warnings.filterwarnings('ignore')
+
+root = r'air_pollution_death_rate_related/data/Air_Pollution/county_features_data/county_features_train/florida_bay_feature.csv'
+
+def test_load_data():
+
+	[X_train, y_train, X_test, y_test], scaler = main_AQI.load_data(root)
+
+	assert X_train.shape == (821, 1, 38)
+	assert y_train.shape == (821,)
+	assert X_test.shape == (205, 1, 38)
+	assert y_test.shape == (205,)
+
+
+# def test_predict_point_by_point():
+
+# 	model_path = "../../Trained_model/county_AQI_model/florida_bay_model.h5"
+# 	model = load_model(model_path)
+# 	[X_train, y_train, X_test, y_test], scaler = main_AQI.load_data(root)
+# 	predictions = helpers.predict_point_by_point(model, X_test)
+# 	assert predictions.shape == (205,)
+
+
